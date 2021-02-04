5741bd8fd91cd8d81ddad171100ab73fb9b7b28c
Matias Blanc <programador.matiasblanc@gmail.com>
2021-02-04 11:18:53 -0300

    version estable pero mejorable

---
+++ Sendero Tecnologico.bat
@@ -0,0 +1,18 @@
+@echo off
+color 2
+mode 61,10
+:a
+title Sendero Tecnologico Bot
+echo   =========================================================
+echo   =                                                       =
+echo   =                  Sendero Tecnologico                  =
+echo   =                                                       =
+echo   =========================================================
+
+cd C:\Users\Matias Blanc\OneDrive\Escritorio\workSpace\botDiscord
+echo.
+echo.
+node server.js
+pause>null
+groot:a
+cls
---
+++ server.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on("ready", () => {
+    console.log("Bot Activado!");
+ }); 
+
+client.on("message", message =>{
+    if (message.content.startsWith("!presupuesto") && message.content.length > 30){
+        message.channel.send("Hola, ya estamos trabajando para recomendarte los mejores componentes y ayudarte en lo que nesecites, te estaremos enviando el resultado final lo antes posible, mucha suerte!! :revolving_hearts: ");
+      }
+
+    else if(message.content.startsWith("!presupuesto") && message.content.length < 30){ 
+        message.channel.send("Hola,muchas gracias por conctactarnos, pero debes darnos mas espesificaciones para poder elegir los mejores componentes para ti, por favor vuelve a escrbir el comando y luego se un poco mas espesifico, por ejemplo podrias colocar algo como !presupuesto de 500$ para una pc de edicion de videos, y nosotros nos ponderemos a trabajar para entregarte tu presupuesto lo antes posible :nerd: :smile: ");
+        }
+        }
+      );
+      
+      client.on("message", message =>{
+        if (message.content.startsWith("!mencion")) {
+          let user = message.mentions.users.first();
+          user.send("Tienes un presupuesto nuevo pendiente!")
+        }
+      })
+      
+       client.login("ODAzNDE0NjY0ODIxMDgwMDc0.YA9cNg.Ad5utkAGalvfAigmdsklj-fMoCg"); 
